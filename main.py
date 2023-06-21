96fb2f3372abffd39772ab7ac92e21dae9016ae7
Shlok Jadhav <shlok.jadhav.07@gmail.com>
2023-06-21 15:24:24 -0700

    commit

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
-
+    male = int(input('Enter number of male students: '))
+    female = int(input('Enter number of female students: '))
+    total = male + female
+    m_perc = (male/total)*100
+    f_perc = (female/total)*100
+    print(f'The total number of students: \t\t {total}')
+    print(f'The number of males and females \t {male} \t {female}')
+    print(f'The percentage of males and females \t {m_perc:.2f}%\t {f_perc:.2f}%')
     """
     ########################################
     # Do not delete the return statement
